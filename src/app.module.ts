4d5df49c2f060038e7dce10d64bf8baeacdebc67
realsashah <ahsan.hameed@solissol.com>
2022-01-24 10:52:16 +0500

    added mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MediaUploadModule } from './components/file-management/media-upload/media-upload.module';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
     MediaUploadModule,
   ],
   controllers: [AppController],
