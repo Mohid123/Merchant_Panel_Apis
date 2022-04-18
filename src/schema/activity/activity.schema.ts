488121bfbaba3361bf25b1f4e22a3d8d8e8761b3
Muhammad Arsalan <muhammad.arsalan3697@gmail.com>
2022-04-18 15:55:19 +0500

    Activity module added

---
+++ src/schema/activity/activity.schema.ts
@@ -0,0 +1,30 @@
+import * as mongoose from 'mongoose';
+import { generateStringId } from 'src/components/file-management/utils/utils';
+import { ActivityInterface } from 'src/interface/activity/activity.interface';
+export const ActivitySchema = new mongoose.Schema(
+  {
+    _id: { type: String, default: generateStringId },
+    activityType: { type: String, default: '' },
+    activityTime: { type: Number },
+    merchantID: { type: String, default: '' },
+    customerID: { type: String, default: '' },
+    dealID: { type: String, default: '' },
+  },
+  {
+    collection: 'activities',
+  },
+);
+
+ActivitySchema.set('timestamps', true);
+ActivitySchema.set('toJSON', {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+    delete ret._id;
+  },
+});
+
+mongoose.model('Activity', ActivitySchema);
+ActivitySchema.pre<ActivityInterface>('save', async function (next) {
+  next();
+});
