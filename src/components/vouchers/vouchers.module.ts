bddcee89f80e9b24ced7e2b2bda46001bead5343
Muhammad Arsalan <muhammad.arsalan3697@gmail.com>
2022-04-22 16:19:53 +0500

    voucher module added

--- src/components/vouchers/vouchers.module.ts
+++ src/components/vouchers/vouchers.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { VoucherSchema } from 'src/schema/vouchers/vouchers.schema';
+import { VouchersController } from './vouchers.controller';
+import { VouchersService } from './vouchers.service';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Voucher', schema: VoucherSchema }]),
+  ],
+  controllers: [VouchersController],
+  providers: [VouchersService],
+})
 export class VouchersModule {}
