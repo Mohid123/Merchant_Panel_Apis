7504cf63c83a17e4c6baa68dc7f4d4aa64b3b328
Haider <haidermalik1996@gmail.com>
2022-05-17 12:44:22 +0500

    cors error handeled

--- src/components/auth/auth.service.ts
+++ src/components/auth/auth.service.ts
@@ -14,6 +14,7 @@ import * as nodemailer from 'nodemailer';
 
 var htmlencode = require('htmlencode');
 var generator = require('generate-password');
+var otpGenerator = require('otp-generator');
 
 let transporter;
 
