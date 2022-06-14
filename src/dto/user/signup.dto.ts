a090959c868a7a82ff31b5b7f2f72bd1a2217618
Mohid123456 <mohid.dastgeer@solissol.com>
2022-06-14 13:26:31 +0500

    small fix

--- src/dto/user/signup.dto.ts
+++ src/dto/user/signup.dto.ts
@@ -1,22 +1,16 @@
 /* eslint-disable prettier/prettier */
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
-import { businessHour } from "../../interface/user/users.interface";
+import { IsNotEmpty } from 'class-validator';
 
 export class SignUpDTO {
-
-    //Credentials
 
     @ApiProperty()
     @IsNotEmpty()
     email: string;
 
-    // @ApiProperty()
-    // @IsNotEmpty()
-    // @IsString()
-    // @MinLength(5)
-    // @MaxLength(1024)
-    // password: string;
+    @ApiProperty()
+    @IsNotEmpty()
+    password: string;
 
     // Personal Details
 
@@ -41,13 +35,13 @@ export class SignUpDTO {
     businessType: string[];
 
     @ApiProperty()
-    legalName: string;
+    companyName: string;
 
     @ApiProperty()
     streetAddress: string;
 
     @ApiProperty()
-    zipCode: number;
+    zipCode: string;
 
     @ApiProperty()
     city: string;
@@ -57,72 +51,5 @@ export class SignUpDTO {
 
     @ApiProperty()
     website_socialAppLink: string;
-
-    @ApiProperty()
-    newUser: boolean;
-
-    //Business Hours
-
-    @ApiProperty({
-        example: [
-            {
-                day: 'MN',
-                firstStartTime: '10:00',
-                firstEndTime: '13:00',
-                secondStartTime: '14:00',
-                secondEndTime: '18:00',
-                isWorkingDay: false,
-            },
-            {
-                day: 'TU',
-                firstStartTime: '10:00',
-                firstEndTime: '13:00',
-                secondStartTime: '14:00',
-                secondEndTime: '18:00',
-                isWorkingDay: false,
-            },
-            {
-                day: 'WD',
-                firstStartTime: '10:00',
-                firstEndTime: '13:00',
-                secondStartTime: '14:00',
-                secondEndTime: '18:00',
-                isWorkingDay: false,
-            },
-            {
-                day: 'TH',
-                firstStartTime: '10:00',
-                firstEndTime: '13:00',
-                secondStartTime: '14:00',
-                secondEndTime: '18:00',
-                isWorkingDay: false,
-            },
-            {
-                day: 'FR',
-                firstStartTime: '10:00',
-                firstEndTime: '13:00',
-                secondStartTime: '14:00',
-                secondEndTime: '18:00',
-                isWorkingDay: false,
-            },
-            {
-                day: 'SA',
-                firstStartTime: '10:00',
-                firstEndTime: '13:00',
-                secondStartTime: '14:00',
-                secondEndTime: '18:00',
-                isWorkingDay: false,
-            },
-            {
-                day: 'SU',
-                firstStartTime: '10:00',
-                firstEndTime: '13:00',
-                secondStartTime: '14:00',
-                secondEndTime: '18:00',
-                isWorkingDay: false,
-            },
-        ]
-    })
-    businessHours: businessHour[];
 
 }
