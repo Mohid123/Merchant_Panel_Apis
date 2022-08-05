89743e2a0bc4a54d7a425af7f5b23f5686282ec8
Ahsan Shah <shahahsan56@gmail.com>
2022-08-05 14:27:39 +0500

    removed leadSource in approveMerchant

--- src/components/users/users.service.ts
+++ src/components/users/users.service.ts
@@ -616,6 +616,8 @@ export class UsersService {
         lowerCaseAlphabets: false,
         specialChars: false,
       });
+      
+      delete approveMerchantDto.leadSource;
 
       approveMerchantDto.legalName = approveMerchantDto.companyName;
       approveMerchantDto.businessType = approveMerchantDto.categoryType;
