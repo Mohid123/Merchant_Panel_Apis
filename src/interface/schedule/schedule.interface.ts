2607ccc1155dd0d20fa365a1fc7bc5a2cc01b07e
Muhammad Arsalan <muhammad.arsalan3697@gmail.com>
2022-08-22 11:00:02 +0500

    Revert "getDealById bug fixed of coverImageURL"
    This reverts commit 4f0aaeb2f2d1c72ddefd23f34c99a2cc715664db.

--- src/interface/schedule/schedule.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from 'mongoose';
-
-export interface Schedule extends Document {
-  _id: string;
-  scheduleDate: Date;
-  status: number; //0 for queue , -1 for unPublish, 1 for published
-  type: string;
-  deletedCheck: boolean;
-}
