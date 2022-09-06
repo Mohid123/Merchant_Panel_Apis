26436f0865a63c01fc29f21f061e5a9740f82b00
Muhammad Arsalan <muhammad.arsalan3697@gmail.com>
2022-09-06 15:49:34 +0500

    uncommented crm in deal

--- src/components/deal/deal.service.ts
+++ src/components/deal/deal.service.ts
@@ -469,9 +469,9 @@ export class DealService implements OnModuleInit {
 
       await this.dealModel.updateOne({ dealID: updateDealDto.dealID }, deal);
 
-      // const res = await axios.get(
-      //   `https://www.zohoapis.eu/crm/v2/functions/createdraftdeal/actions/execute?auth_type=apikey&zapikey=1003.1477a209851dd22ebe19aa147012619a.4009ea1f2c8044d36137bf22c22235d2&dealid=${deal.dealID}`,
-      // );
+      const res = await axios.get(
+        `https://www.zohoapis.eu/crm/v2/functions/createdraftdeal/actions/execute?auth_type=apikey&zapikey=1003.1477a209851dd22ebe19aa147012619a.4009ea1f2c8044d36137bf22c22235d2&dealid=${deal.dealID}`,
+      );
 
       return { message: 'Deal Updated Successfully' };
     } catch (err) {
@@ -4163,7 +4163,7 @@ export class DealService implements OnModuleInit {
       ) {
         throw new Error('Insufficent Quantity of deal present!');
       }
-      debugger
+      debugger;
       let dealVouchers = 0,
         soldVouchers = 0;
       deal.subDeals = deal.subDeals.map((element) => {
