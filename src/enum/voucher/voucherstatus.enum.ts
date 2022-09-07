8a943d2e22cedee632ac631e44a0f007495b7927
Haider <haidermalik1996@gmail.com>
2022-09-07 12:36:48 +0500

    stripe and vat api changed, voucher status updated

--- src/enum/voucher/voucherstatus.enum.ts
+++ src/enum/voucher/voucherstatus.enum.ts
@@ -2,6 +2,6 @@ export enum VOUCHERSTATUSENUM {
   purchased = 'Purchased',
   redeeemed = 'Redeemed',
   expired = 'Expired',
-  refunded = 'Refunded',
-  inDispute = 'In Dispute'
+  // refunded = 'Refunded',
+  // inDispute = 'In Dispute'
 }
